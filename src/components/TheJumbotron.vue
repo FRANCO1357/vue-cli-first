d0e31e9e1cbd5c8475a48a7ac43471fb030dbb18
FRANCO1357 <fmelani13@gmail.com>
2022-07-22 17:39:18 +0200

    title & jumbotron

---
+++ src/components/TheJumbotron.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <img src="../assets/logo.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'TheJumbotron',
+    };
+</script>
+
+<style>
+    img{
+        display: block;
+        width: 200px;
+        margin: 0 auto;
+    }
+</style>
